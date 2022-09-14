0c1259152521ac383726ae95a5df46b0c2090763
davidwills3045 <111367174+davidwills3045@users.noreply.github.com>
2022-09-14 04:45:08 -0700

    Delete close/javascript directory

--- close/javascript/and.js
+++
@@ -1,9 +0,0 @@
-let shoppingCart = document.getElementById('shopping-cart');
-
-document.getElementById('cart-btn').onclick = () =>{
-  if (shoppingCart.style.display === "none") {
-    shoppingCart.style.display = "block";
-  } else {
-    shoppingCart.style.display = "none";
-  }
-}
